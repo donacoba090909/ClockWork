663b5ffd03972da15b71cf83fecaadb10e061e6c
Donzkie <don.acoba@employgroup.com.au>
2018-07-17 15:21:48 +0800

    initial commit

---
+++ Clockwork.API/Controllers/CurrrentEntriesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Clockwork.API.Models;
+using System.Linq;
+
+namespace Clockwork.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class CurrentEntriesController : Controller
+    {
+        // GET api/currententries
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var result = new ClockworkContext();
+            var data = result.CurrentTimeQueries.OrderByDescending(a => a.CurrentTimeQueryId).ToList();
+
+            return Ok(data);
+        }
+
+    }
+}
---
+++ Clockwork.API/Models/Models.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Clockwork.API.Models
+{
+    public class ClockworkContext : DbContext
+    {
+        private static bool _created = false;
+        public DbSet<CurrentTimeQuery> CurrentTimeQueries { get; set; }
+
+        public ClockworkContext()
+        {
+            if (!_created)
+            {
+                _created = true;
+                // Database.EnsureDeleted();
+                Database.EnsureCreated();
+            }
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=clockwork.db");
+            //optionsBuilder.UseSqlite(@"Data Source=d:\Sample.db");
+        }
+    }
+
+    public class CurrentTimeQuery
+    {
+        public int CurrentTimeQueryId { get; set; }
+        public DateTime Time { get; set; }
+        public string ClientIp { get; set; }
+        public DateTime UTCTime { get; set; }
+    }
+}
